708c6c9ea487febffffa3683e2e20d0603442a61
empregaprogresso-eng <empregaprogresso@gmail.com>
2026-01-28 19:55:15 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,36 +1,13 @@
-const CACHE_NAME = 'progresso-online-v5'; // MUDE O NÚMERO SEMPRE
-const urlsToCache = [
-  './',
-  './index.html',
-  './manifest.json',
-  './logo.png',
-  './icon-192.png',
-  './icon-512.png',
-  './empresas/bls.html',
-  './empresas/online.html',
-  './vagas/index.html'
-];
-
-self.addEventListener('install', event => {
+self.addEventListener('install', () => {
   self.skipWaiting();
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
-  );
 });
 
 self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
-      )
-    )
-  );
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(res => res || fetch(event.request))
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.map(key => caches.delete(key))
+      );
+    })
   );
 });
